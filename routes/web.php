b5ba76691050aafbdfd86fec2905a83b130e993a
Fikriarkani <77955970+Fikriarkani@users.noreply.github.com>
2023-05-19 16:22:05 +0700

    penyamaan routes/web.php dengan repo

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('front/login');
-});
+Route::resource('landingPage', MainController::class);
+
+Route::get("sesi", [SessionController::class, "index"]);
+Route::post("/sesi/login", [SessionController::class, "login"]);
+Route::get("/sesi/logout", [SessionController::class, "logout"]);
+Route::post("/sesi/create", [SessionController::class, "create"]);
+Route::get("/sesi/register", [SessionController::class, "register"]);
