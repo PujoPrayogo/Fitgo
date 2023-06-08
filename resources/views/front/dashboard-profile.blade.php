12daff90988a4594a4e1cb4691715ccb2e25d30d
pujo prayogo <pujoprayogo2@gmail.com>
2023-06-08 20:36:06 +0700

    profile final

--- resources/views/front/dashboard-profile.blade.php
+++ resources/views/front/dashboard-profile.blade.php
@@ -1,29 +1,25 @@
 @extends('layout/profile-aplikasi')
 
-@section('profile-picture')
-https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png
-@endsection
-
 @section('username')
 Mamat
 @endsection
 
-@section('weight')
-80
-@endsection
-
-@section('height')
-180
-@endsection
-
-@section('bmi')
-24
+@section('email')
+pujoganteng@gmail.com
 @endsection
 
 @section('age')
 21
 @endsection
 
-@section('bodygoal')
-Lose Weight
+@section('height')
+180
+@endsection
+
+@section('weight')
+80
+@endsection
+
+@section('bmi')
+24
 @endsection
